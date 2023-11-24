a4a906b12faf41edfa210c850f0a2a8fe07c91e6
Aymeric Felisiak <aymeric.felisiak@gmail.com>
2023-11-24 11:01:42 +0100

    Added sign-in page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/home/Home';
 import Footer from './components/footer/Footer';
+import SignIn from './pages/sign-in/SignIn';
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Home />} />
+                <Route path="/sign-in" element={<SignIn />} />
             </Routes>
         </BrowserRouter>
         <Footer />
