a8a14d2fe7e993aab278950db3d90efe87a45803
Aymeric Felisiak <aymeric.felisiak@gmail.com>
2023-11-24 10:48:04 +0100

    Added home and footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './style/sass/style.scss'
 import Navbar from "./components/navbar/Navbar";
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/home/Home';
+import Footer from './components/footer/Footer';
 
 
 function App() {
     return <div className="App">
         <Navbar />
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+            </Routes>
+        </BrowserRouter>
+        <Footer />
     </div>;
 }
 
