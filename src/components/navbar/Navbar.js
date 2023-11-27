8cc78c8a0e1646e8a1cbe7868244fc1e0c7e1370
Aymeric Felisiak <aymeric.felisiak@gmail.com>
2023-11-27 09:42:09 +0100

    Added logged in navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ArgentBankLogo from '../../img/argentBankLogo.png';
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 export default function Navbar() {
+    const user = useSelector((state) => state.user);
+
     return (
         <nav className="main-nav">
             <Link className="main-nav-logo" to="/">
@@ -13,12 +16,25 @@ export default function Navbar() {
                 />
                 <h1 className="sr-only">Argent Bank</h1>
             </Link>
-            <div>
-                <Link to="/login" className="main-nav-item">
-                    <i className="fa fa-user-circle"></i>
-                    Sign In
-                </Link>
-            </div>
+            {user.user ? (
+                <div className='main-nav-item-wrapper'>
+                    <Link to="/login" className="main-nav-item">
+                        <i className="fa fa-user-circle"></i>
+                        <span>{user.user.body.firstName}</span>
+                    </Link>
+                    <Link to="/" className="main-nav-item">
+                        <i className="fa fa-sign-out"></i>
+                        <span>Sign Out</span>
+                    </Link>
+                </div>
+            ) : (
+                <div className='main-nav-item-wrapper'>
+                    <Link to="/login" className="main-nav-item">
+                        <i className="fa fa-user-circle"></i>
+                        <span>Sign In</span>
+                    </Link>
+                </div>
+            )}
         </nav>
     );
 }
